9c61b932fffb5999711b64384a1f543b3c963655
Dylan H <dylanharris6000@gmail.com>
2023-03-24 20:18:43 -0400

    Initial commit

---
+++ src/main/java/net/djh/djhguns/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.djh.djhguns.mixin;
+
+import net.djh.djhguns.GunMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		GunMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
